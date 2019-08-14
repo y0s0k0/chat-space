5a1647bf94b4eb99ee2ecd2b820992ab5733817e
y0s0k0 <y0s0k0.0521@gmail.com>
2019-08-14 11:27:20 +0900

    add-asynchronous-communication#2

--- app/assets/javascripts/create.js
+++ app/assets/javascripts/create.js
@@ -1,27 +1,23 @@
 $(function() {
   function buildHTML(message) {
 
-    if(message.image == null) {
-      message_image = ""
-    } else {
-      message_image = `<img class="lower-message__image" src="${message.image}">`
-    };
+    message_image = message.image == null ? "" : `<img class="lower-message__image" src="${message.image}">`;
 
     var html = `<div class="message">
-                <div class="upper-message">
-                <div class="upper-message__user-name">
-                ${message.user_name}
-                </div>
-                <div class="upper-message__date">
-                ${message.created_at}
-                </div>
-                </div>
-                <div class="lower-message">
-                <p class="lower-message__content">
-                ${message.body}
-                </p>
-                </div>
-                ${message_image}
+                  <div class="upper-message">
+                    <div class="upper-message__user-name">
+                      ${message.user_name}
+                    </div>
+                    <div class="upper-message__date">
+                      ${message.created_at}
+                    </div>
+                  </div>
+                  <div class="lower-message">
+                    <p class="lower-message__content">
+                      ${message.body}
+                    </p>
+                  </div>
+                  ${message_image}
                 </div>`
                 
     return html;
@@ -44,7 +40,7 @@ $(function() {
       var html = buildHTML(data);
       $(".messages").append(html);
       $(".messages").animate({scrollTop: $(".messages")[0].scrollHeight});
-      $(".input-box__text").val("");
+      $("form")[0].reset();
     })
     .fail(function(data){
       alert("メッセージを入力してください");
